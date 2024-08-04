5ab7fd928fc89087d46f5f630c2a325c572f7a31
Amirshah98760 <amirshah98760@gmail.com>
2024-08-04 23:00:59 +0500

    react static website

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import FoodMenuList from './components/FoodMenuList';
+import ExploreFoodList from './components/ExploreFoodList';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <ExploreFoodList />
+      <FoodMenuList />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
